200ed8ad8c5660779b8ed53d09ba3709cce76956
Daniel Carroll <danielc103@gmail.com>
2015-07-08 21:25:11 -0400

    comments

--- app/src/main/java/com/fsu/tictacnolebt/BluetoothConnect.java
+++ app/src/main/java/com/fsu/tictacnolebt/BluetoothConnect.java
@@ -149,17 +149,25 @@ public class BluetoothConnect extends MainActivity {
 
     }
 
-
+    /*
+    Connect thread class -
+     */
     private class ConnectThread extends Thread {
         private final BluetoothSocket mmSocket;
         private final BluetoothDevice mmDevice;
 
+        /*
+        Constructor - store UUID in tmp socket
+         */
         public ConnectThread(BluetoothDevice device){
 
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
             BluetoothSocket tmp = null;
             mmDevice = device;
-
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
             try{
+                // MY_UUID is the app's UUID string, also used by the server code
                 tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -167,11 +175,18 @@ public class BluetoothConnect extends MainActivity {
             mmSocket = tmp;
         }
 
-
+        /*
+        ConnectThread run, will try to connect through socket
+         */
         public void run(){
+
+            // Cancel discovery because it will slow down the connection
             mBluetoothAdapter.cancelDiscovery();
 
             try {
+
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
                 mmSocket.connect();
 
                 //pass socket
@@ -182,10 +197,13 @@ public class BluetoothConnect extends MainActivity {
                 }catch (IOException closeException){}
                 return;
             }
-
+            // Do work to manage the connection (in a separate thread)
             //manageConnectedSocket(mmSocket);
         }
 
+        /*
+        will cancel an in progress connection and close socket
+         */
         public void cancel(){
             try {
                 mmSocket.close();
